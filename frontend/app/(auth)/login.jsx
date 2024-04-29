36864523e0382620ac3aa26e5de876c0c2070878
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-29 12:03:46 +0300

    added-InputBox-component

--- frontend/app/(auth)/login.jsx
+++ frontend/app/(auth)/login.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native';
+// Dependencies
 import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+// Components
 import AppName from '../../components/AppName';
+import InputBox from '../../components/InputBox';
 
 const Login = () => {
   return (
-    <View>
-      <AppName/>
-    </View>
+    <SafeAreaView>
+      <AppName />
+      <Text className='font-urbanistLight text-lg ml-[29]'>Let's get back on track in no time</Text>
+
+      <InputBox />
+    </SafeAreaView>
   )
 }
 
---
+++ frontend/components/InputBox.jsx
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const InputBox = () => {
+  return (
+    <View>
+        <Text className='text-base text-[#ACACAC] font-urbanist-Light'>Field</Text>
+    </View>
+  )
+}
+
+export default InputBox;
