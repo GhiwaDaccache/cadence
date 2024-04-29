147156cbf643ef809b708d9d6defc552bce7966b
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-29 23:42:44 +0300

    added-registration-inputs

--- frontend/app/(auth)/registration.jsx
+++ frontend/app/(auth)/registration.jsx
@@ -3,7 +3,12 @@ import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AppName from '../../components/AppName';
 import GreyInputBox from '../../components/GreyInputBox'
+import {useRegistrationLogic} from './registration-logic';
+
+
 const Registration = () => {
+  const { record, setRecord } = useRegistrationLogic();
+
   return (
     <SafeAreaView className='bg-white h-full'>
       <AppName
@@ -12,9 +17,20 @@ const Registration = () => {
       <Text className='font-urbanistLight text-lg ml-[29] mb-[45]'>Welcome to Cadence! Let's get to {"\n"}know you first</Text>
       <GreyInputBox
         title={'First name'} 
-        placeholder={'John Doe'}
-      
-      
+        placeholder={'John'}
+        handleChange={(e)=>{
+          setRecord({ ...record, firstName: e});
+        }}
+        value={record.firstName}
+      />
+
+      <GreyInputBox
+        title={'Last name'} 
+        placeholder={'Doe'}
+        handleChange={(e)=>{
+          setRecord({ ...record, lastName: e});
+        }}
+        value={record.lastName}
       />
       
       
