fc776a896111afd4917a95ab35fc95b64d1f8cf9
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-30 01:28:06 +0300

    added-start-run-ui

--- frontend/app/(tabs)/start-run.jsx
+++ frontend/app/(tabs)/start-run.jsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
+import LogoSmall from '../../assets/images/LogoSmall';
 
 const StartRun = () => {
   return (
-    <View>
-      <Text>Start run</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full pt-[80] px-[29] flex items-center'>
+      <LogoSmall/>
+      <Text className='font-urbanistBold text-2xl pt-4'>00:00:00</Text>
+
+      <View className='flex flex-row justify-between w-full pt-[40]'>
+
+        <View>
+          <Text className='text-base font-urbanist'>Distance (km)</Text>
+          <Text className='text-2xl font-urbanistBold'>00.00</Text>
+        </View>
+
+        <View>
+          <Text className='text-base font-urbanist'>Avg. pace (min/km)</Text>
+          <Text className='text-2xl font-urbanistBold'>00:00</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
