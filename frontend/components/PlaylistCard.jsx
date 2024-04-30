0b016362bf9d563b64927e3555df74531b20c36f
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-30 23:04:51 +0300

    frontend-changed-Playlist-component

--- frontend/components/PlaylistCard.jsx
+++ frontend/components/PlaylistCard.jsx
@@ -5,16 +5,16 @@ import { View, Text, Image } from 'react-native';
 // Assets
 import images from '../assets/images/images';
 
-const PlaylistCard = () => {
+const PlaylistCard = ({ image, title, time, level}) => {
   return (
     <View className='flex flex-row pt-6 self-start pl-[29]'>
         <Image 
-          source={images.playlist}
+          source={image}
         />
         <View className='flex justify-center pl-4'>
-          <Text className='font-urbanistsemibold text-base'>Monday runs</Text>
-          <Text className=' pt-6'>Intermediate</Text>
-          <Text>20:03</Text>
+          <Text className='font-urbanistsemibold text-base'>{title}</Text>
+          <Text className=' pt-6'>{level}</Text>
+          <Text>{time}</Text>
         </View>
       </View>
   )
