451f01b30935a46829e23a635033c5b870898c4a
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-30 13:41:41 +0300

    frontend-imported-new-icons

--- frontend/app/(tabs)/profile.jsx
+++ frontend/app/(tabs)/profile.jsx
@@ -13,27 +13,30 @@ const Profile = () => {
   return (
     <SafeAreaView className='bg-white h-full flex items-center'>
       <StatusBar backgroundColor='white'/>
-      <View className='w-full h-2/5'>
+      <View className='w-full h-[250]'>
         <Image source={images.cover} className='h-full w-full'  />
       </View>
 
-      <View className='absolute z-10 top-56 left-10 w-24 h-24'>
+      <View className='absolute z-10 top-48 left-[29] w-24 h-24'>
         <Image source={images.profile}  className='h-full w-full'  />
       </View>
 
       <Text className='font-usemibold text-lg'>John Doe</Text>
       <View className='flex flex-row w-full pt-10 px-[29] justify-between'>
         <ProfileCard
+          // source={'previous'}
           title={'Previous'}
           icon={icons.history} 
         />
 
         <ProfileCard
+          // source={'previous'}
           title={'My plan'}
           icon={icons.history} 
         />
 
         <ProfileCard
+          // source={'previous'}
           title={'Badges'}
           icon={icons.history} 
         />
--- frontend/assets/icons/icons.js
+++ frontend/assets/icons/icons.js
@@ -1,13 +1,17 @@
 import run from "../icons/run.png";
+import medal from "../icons/medal.png";
 import profile from "../icons/profile.png";
 import compass from "../icons/compass.png";
 import setting from "../icons/setting.png";
 import history from "../icons/history.png";
+import calendar from "../icons/calendar.png";
 
 export default {
     profile,
     run,
     compass,
     setting,
-    history
+    history,
+    calendar,
+    medal
 }
