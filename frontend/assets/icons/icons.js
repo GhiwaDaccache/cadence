7ef18c3078d60e75c62c122bc103a44d08961bcb
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-01 14:22:43 +0300

    frontend-imported-icons

--- frontend/assets/icons/icons.js
+++ frontend/assets/icons/icons.js
@@ -1,5 +1,6 @@
 import run from "../icons/run.png";
 import dot from "../icons/dot.png";
+import road from "../icons/road.png";
 import medal from "../icons/medal.png";
 import timer from "../icons/timer.png";
 import trophy from "../icons/trophy.png";
@@ -10,6 +11,7 @@ import setting from "../icons/setting.png";
 import history from "../icons/history.png";
 import calendar from "../icons/calendar.png";
 import distance from "../icons/distance.png";
+import schedule from "../icons/schedule.png";
 
 export default {
     profile,
@@ -23,5 +25,7 @@ export default {
     sunrise,
     timer,
     trophy,
-    dot
+    dot,
+    road,
+    schedule
 }
