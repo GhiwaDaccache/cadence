242ec08ecc90ffd3deb3e0ea28f9989165f1c1a7
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-01 18:16:59 +0300

    frontend-added-NextRun-section

--- frontend/components/NextRun.jsx
+++ frontend/components/NextRun.jsx
@@ -1,27 +1,35 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import icons from '../assets/icons/icons'
-
+import PlaylistCard from '../components/PlaylistCard'
+import images from '../assets/images/images'
 const NextRun = () => {
   return (
-    <View className='flex py-4 px-6 mx-7 mt-7 bg-grey rounded-md'>
-        <Text className='font-urbanistBold text-base'>Next run</Text>
-          <Text className='font-urbanist text-base'>Distance</Text>
-            <View className='flex flex-row items-center'>
-              <Text className='font-urbanist text-base pr-3'>2.9 km</Text>
-              <Image 
-                source={icons.road} 
-                className='w-5 h-5'  
-              />
-            </View>
+    <View className='flex py-4 px-6 mx-7 mt-5 bg-grey rounded-md'>
+      <Text className='font-urbanistBold text-base'>Next run</Text>
 
-            <View className='py-2 px-3 mt-5 bg-white rounded-sm'>
-                <Text>Week 2 Day 2:</Text>
-                <Text>Intervals</Text>
-                <View className='w-full bg-black rounded-md h-4'>
+      <View className='flex flex-row justify-between'>
+        <Text className='font-urbanist text-base'>Distance</Text>
+        <View className='flex flex-row items-center'>
+          <Text className='font-urbanist text-base pr-3'>2.9 km</Text>
+          <Image 
+            source={icons.road} 
+            className='w-5 h-5'  
+          />
+        </View>
+      </View>
 
-                </View>
-            </View>
+      <View className='py-2 px-3 mt-2 bg-white rounded-sm'>
+        <Text className='font-urbanist text-base'>Week 2 Day 2</Text>
+        <Text className='font-urbanist text-base pt-1'>Intervals</Text>
+        <View className='w-full bg-black rounded-md h-4 mt-2'></View>
+        <PlaylistCard 
+          image={images.playlist}
+          level={'Intermediate'}
+          time={'20:41'}
+          title={'Monday run'}
+        />
+      </View>
       </View>
   )
 }
