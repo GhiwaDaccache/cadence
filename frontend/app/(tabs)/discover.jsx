a1a70317636c7b8e0f68bf389135e3956f8e163b
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-01 11:36:58 +0300

    frontend-edited-Badge-component

--- frontend/app/(tabs)/discover.jsx
+++ frontend/app/(tabs)/discover.jsx
@@ -1,14 +1,9 @@
 import { View } from 'react-native';
 import React from 'react';
 
-
-
-
 const Discover = () => {
   return (
-    <View >
-      
-
+    <View>
     </View>
   )
 }
--- frontend/components/Badge.jsx
+++ frontend/components/Badge.jsx
@@ -2,23 +2,20 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
 
-//Assets
-import icons from '../assets/icons/icons';
-
-const Badge = () => {
+const Badge = ({ icon, title, date, distance, timing}) => {
   return (
     <View className='bg-grey flex flex-row w-[303] px-9 py-2 rounded-md'>
         <Image 
-          source={icons.trophy} 
+          source={icon} 
           resizeMode="contain" 
           className='w-14 h-14 self-center'
         />
 
       <View className='pl-8'>
-        <Text className='text-base font-urbanistBold pb-1'>First run</Text>
-        <Text className='text-base font-urbanistLight'>15-08-2023</Text>
-        <Text className='text-base font-urbanistLight'>3.81 km</Text>
-        <Text className='text-base font-urbanistLight'>30:12</Text>
+        <Text className='text-base font-urbanistBold pb-1'>{title}</Text>
+        <Text className='text-base font-urbanistLight'>{date}</Text>
+        <Text className='text-base font-urbanistLight'>{distance} km</Text>
+        <Text className='text-base font-urbanistLight'>{timing}</Text>
       </View>
     </View>
   )
