f0c26b5ac8fa362a55e7a88aeb50aaa4505e4a7b
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-01 18:23:26 +0300

    frontend-fixed-NextRun

--- frontend/components/NextRun.jsx
+++ frontend/components/NextRun.jsx
@@ -1,9 +1,15 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import icons from '../assets/icons/icons'
-import PlaylistCard from '../components/PlaylistCard'
-import images from '../assets/images/images'
-const NextRun = () => {
+// Dependencies
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+// Assets
+import icons from '../assets/icons/icons';
+import images from '../assets/images/images';
+
+// Components
+import PlaylistCard from '../components/PlaylistCard';
+
+const NextRun = ({ distance, scheduleDate }) => {
   return (
     <View className='flex py-2 px-6 mx-7 mt-5 bg-grey rounded-md'>
       <Text className='font-urbanistBold text-base'>Next run</Text>
@@ -11,7 +17,7 @@ const NextRun = () => {
       <View className='flex flex-row justify-between'>
         <Text className='font-urbanist text-base'>Distance</Text>
         <View className='flex flex-row items-center'>
-          <Text className='font-urbanist text-base pr-3'>2.9 km</Text>
+          <Text className='font-urbanist text-base pr-3'>{distance} km</Text>
           <Image 
             source={icons.road} 
             className='w-5 h-5'  
@@ -19,10 +25,10 @@ const NextRun = () => {
         </View>
       </View>
 
-      <View className='py-2 px-3 mt-2 bg-white rounded-sm'>
-        <Text className='font-urbanist text-base'>Week 2 Day 2</Text>
+      <View className='py-2 px-3 mt-2 bg-white rounded-md'>
+        <Text className='font-urbanist text-base'>{scheduleDate}</Text>
         <Text className='font-urbanist text-base pt-1'>Intervals</Text>
-        <View className='w-full bg-black rounded-md h-4 mt-2'></View>
+        <View className='w-full bg-black rounded h-4 mt-2'></View>
         <PlaylistCard 
           image={images.playlist}
           level={'Intermediate'}
