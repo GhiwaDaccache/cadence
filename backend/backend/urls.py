27bfa6de9706d24f1bf870a9aecff96a58216b79
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-02 12:01:39 +0300

    backend-created-test-API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("cadence/", include("cadence.urls")),
     path('admin/', admin.site.urls),
+    path("cadence/", include("cadence.urls")),
 ]
