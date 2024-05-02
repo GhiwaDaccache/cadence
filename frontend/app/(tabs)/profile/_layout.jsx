7074dc86d0385652a337ac7ce397f4f9e0d21a91
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-02 14:44:53 +0300

    frontend-added-pages-header

--- frontend/app/(tabs)/profile/_layout.jsx
+++ frontend/app/(tabs)/profile/_layout.jsx
@@ -7,7 +7,12 @@ const ProfileLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="profile" options={{ headerShown: false}} />
-      <Stack.Screen name="badges" options={{ headerShown: false}} />
+      <Stack.Screen name="badges" 
+        options={{
+          title: 'Badges',          
+        }}
+      
+      />
       <Stack.Screen name="plan" options={{ headerShown: false}} />
       <Stack.Screen name="previous" options={{ headerShown: false}} />
     </Stack>
