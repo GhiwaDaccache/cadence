8eaf8e9dc189e763ca043cb7e2ff8cce0237dc0e
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-03 17:46:15 +0300

    backend-fixed-user-model

--- backend/cadence/models/Usermodel.py
+++ backend/cadence/models/Usermodel.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User;
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = name = models.CharField(max_length=255, null=True)
-    cover_photo = name = models.CharField(max_length=255, null=True)
+    profile_photo = models.CharField(max_length=255, null=True)
+    cover_photo = models.CharField(max_length=255, null=True)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE, null=True)
 
--- backend/cadence/models/__init__.py
+++ backend/cadence/models/__init__.py
@@ -1,5 +1,6 @@
 from .RunModel import Run;
 from .PlanModel import Plan;
 from .SegmentModel import Segment;
+from .Usermodel import UserProfile;
 from .PlaylistModel import Playlist;
 from .SpotifySongModel import SpotifySong;
