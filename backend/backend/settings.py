f88d05d0a4a64149cd12d4a371376716f75e5791
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-03 14:50:25 +0300

    backend-added-jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ) 
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,10 +82,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'cadence_db',
+'USER': 'root',
+'PASSWORD': '',
+'HOST': 'localhost',
+'PORT': '3306',
+}
 }
 
 
