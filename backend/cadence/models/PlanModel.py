0e24622e6acb75e6fe83cc20633b67c46f44ce4d
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-03 15:12:22 +0300

    backend-created-plan-model

---
+++ backend/cadence/models/PlanModel.py
@@ -0,0 +1,11 @@
+from django.db import models;
+from django.core.validators import MinValueValidator;
+
+class Plan(models.Model):
+    name = models.CharField(max_length=255)
+    duration = models.IntegerField(validators=[MinValueValidator(0)])
+    level = models.CharField(max_length=100)
+    image = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'plans'
