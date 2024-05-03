b0b05ec7e0f365e3714b293ebdd9808bffc5a1e5
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-03 14:45:22 +0300

    backend-reinitialized-backend

--- backend/cadence/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .runModel import Run;
-from .planModel import Plan;
-from .playlistModel import Playlist;
-from .spotifySongsModel import SpotifySongs;
--- backend/cadence/models/playlistModel.py
+++
@@ -1,7 +0,0 @@
-from django.db import models;
-
-class Playlist(models.Model):
-    name = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'playlists'
