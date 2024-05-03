976a6f8e58e5e87dc445655846360fbaa89af9c2
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-03 11:17:52 +0300

    backend-created-songs-model

---
+++ backend/cadence/models/spotifySongsModel.py
@@ -0,0 +1,10 @@
+from django.db import models;
+from .playlistModel import Playlist;
+
+class SpotifySongs(models.Model):
+    name = models.CharField(max_length=255)
+    spotify_id = models.CharField(max_length=255)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, on_update=models.CASCADE)
+
+    class Meta:
+        db_table = 'spotify_songs'
