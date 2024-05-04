69e3d070411c280915a333fae86125c96ed871ea
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-04 23:33:34 +0300

    backend-added-create-plan-api

--- backend/cadence/urls.py
+++ backend/cadence/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views.UserViews import *
+from .views.PlanViews import *
 
 urlpatterns = [
     path('', User.as_view()), 
     path('api/user/create/', create_user, name='create_user'),
     path('api/user/<int:user_id>/', get_user_by_id, name='get_user_by_id'),
     path('api/user/<int:user_id>/edit/', edit_user, name='edit_user'),
+    path('api/plan/', Plan.add_plan, name='add_plan'),
 ]
--- backend/cadence/views/PlanViews.py
+++ backend/cadence/views/PlanViews.py
@@ -1,12 +1,22 @@
-# views.py
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from ..models.PlanModel import Plan
 from ..serializers.PlanSerializer import PlanSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+class Plan(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    @api_view(['POST'])
+    def add_plan(request):
+        serializer = PlanSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+    
