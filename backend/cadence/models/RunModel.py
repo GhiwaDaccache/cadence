85bdbc22eb3aba4a268303b5a854780a7bdd5648
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-06 13:19:31 +0300

    backend-edited-run-model

--- backend/cadence/models/RunModel.py
+++ backend/cadence/models/RunModel.py
@@ -3,9 +3,9 @@ from .PlaylistModel import Playlist;
 
 class Run(models.Model):
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
-    avg_pace = models.IntegerField()
+    avg_pace = models.FloatField()
     duration = models.IntegerField()
-    distance = models.IntegerField()
+    distance = models.FloatField()
 
     class Meta:
         db_table = 'runs'
