768acef29f3fc993e614c9f91e7030983e50d856
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-07 16:00:10 +0300

    backend-changed-routes-urls

--- backend/cadence/urls.py
+++ backend/cadence/urls.py
@@ -13,23 +13,36 @@ from .views.RecordedRunViews import *
 from .views.FavoritePlaylistViews import *
 
 urlpatterns = [
+    # User
     path('api/user/register/', RegistrationViews.as_view(), name='register'),
-    path('api/user/', UserViews.as_view()),
-    path('api/plan/', PlanViews.as_view()),
-    path('api/plan/<int:pk>/', PlanViews.as_view()),
+    path('api/user/', UserViews.as_view(), name='user'),
+
+    # Plan
+    path('api/plan/', PlanViews.as_view(), name='plans'),
+    path('api/plan/<int:pk>/', PlanViews.as_view(), name='plan_id'),
+
+    # Plan run
+    path('api/plan_run/', PlanRunViews.as_view(), name='plan_runs'),
+    path('api/plan_run/<int:pk>', PlanRunViews.as_view(), name='plan_run_id'),
+
+    # Badge
+    path('api/badge/', BadgeViews.as_view(), name='badge'),
+    path('api/earned_badge/', EarnedBadgeViews.as_view(), name='earned_badge'),
+
+    # Favorite playlist
+    path('api/favorite_playlist/', FavoritePlaylistViews.as_view(), name='favorite_playlist'),
+    
 
     path('', PlaylistViews.as_view()),
+
     path('api/playlist/', PlaylistViews.as_view(), name='add_playlist'),
     # path('api/playlist/get_all_playlists/', PlaylistViews.get_all_playlists, name='get_all_playlists'),
     # path('api/playlist/delete_playlist/<int:id>', PlaylistViews.delete_playlist, name='delete_playlist'),
     # path('api/playlist/get_playlist_by_id/<int:id>', PlaylistViews.get_playlist_by_id, name='get_playlist_by_id'),
-    path('api/badge/', BadgeViews.as_view()),
-    path('api/earned_badge/', EarnedBadgeViews.as_view(), name='earned_badge'),
+    
     path('api/run/', RunViews.as_view()),
     path('api/run/<int:id>', RunViews.as_view()),
-    path('api/favorite_playlist/', FavoritePlaylistViews.as_view()),
     path('api/recorded_run/', RecordedRunViews.as_view()),
-    path('api/plan_run/', PlanRunViews.as_view()),
-    path('api/plan_run/<int:pk>', PlanRunViews.as_view()),
+    
 ]
 
--- backend/cadence/views/EarnedBadgeViews.py
+++ backend/cadence/views/EarnedBadgeViews.py
@@ -19,10 +19,12 @@ class EarnedBadgeViews(APIView):
     def post(self, request):
         try:
             serializer = EarnedBadgeSerializer(data=request.data)
+
             if serializer.is_valid():
                 serializer.save()
                 return Response({'message': 'Earned badge added successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
             return Response({'message': 'Failed to add earned badge', 'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
         except Exception as error:
             return Response({'message': 'Failed to add earned badge', 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
@@ -31,11 +33,13 @@ class EarnedBadgeViews(APIView):
             user_id = request.user.id
             badge_id = request.data.get('badge')
             earned_badge = EarnedBadge.objects.filter(recorded_run__user_id=user_id, badge_id=badge_id).first()
+
             if earned_badge:
                 earned_badge.delete()  
                 return Response({'message': 'Badge removed.'}, status=status.HTTP_204_NO_CONTENT)
             else:
                 return Response({'message': 'Badge not found'}, status=status.HTTP_404_NOT_FOUND)
+            
         except Exception as error:
             return Response({'message': 'Failed to delete badge', 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
