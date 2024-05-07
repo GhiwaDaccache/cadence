a545f2a66ec367f809a678a5e23751d40f8e3ce1
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-07 16:05:39 +0300

    backend-merged-getplaylistss-apis

--- backend/cadence/urls.py
+++ backend/cadence/urls.py
@@ -32,10 +32,10 @@ urlpatterns = [
     # Favorite playlist
     path('api/favorite_playlist/', FavoritePlaylistViews.as_view(), name='favorite_playlist'),
     
+    # Playlist
+    path('api/playlist/', PlaylistViews.as_view(), name='playlist'),
+    path('api/playlist/<int:pk>/', PlaylistViews.as_view(), name='playlist_id'),
 
-    path('', PlaylistViews.as_view()),
-
-    path('api/playlist/', PlaylistViews.as_view(), name='add_playlist'),
     # path('api/playlist/get_all_playlists/', PlaylistViews.get_all_playlists, name='get_all_playlists'),
     # path('api/playlist/delete_playlist/<int:id>', PlaylistViews.delete_playlist, name='delete_playlist'),
     # path('api/playlist/get_playlist_by_id/<int:id>', PlaylistViews.get_playlist_by_id, name='get_playlist_by_id'),
--- backend/cadence/views/PlaylistViews.py
+++ backend/cadence/views/PlaylistViews.py
@@ -23,33 +23,29 @@ class PlaylistViews(APIView):
                 serializer.save()
                 return Response({'message': 'Playlist added successfully.', 'data': serializer.data}, status=status.HTTP_201_CREATED)
             return Response({'message': 'Failed to add playlist.', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    # # works without authentication 
-    # @api_view(['GET'])
-    # def get_all_playlists(request):
-    #     try:
-    #         playlists = Playlist.objects.all()
-    #         serializer = PlaylistSerializer(playlists, many=True)
-    #         return Response(serializer.data)
-    #     except Exception as error:
-    #         return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def get(self, request, pk=None):  
+        try:
+            if pk is not None:
+                playlist = Playlist.objects.get(pk=pk)
+                serializer = PlaylistSerializer(playlist)
+                return Response({'message': 'Success.', 'data': serializer.data}, status=status.HTTP_200_OK)
+            
+            else:
+                playlists = Playlist.objects.all()
+                serializer = PlaylistSerializer(playlists, many=True)
+                return Response({'message': 'success.', 'data': serializer.data}, status=status.HTTP_200_OK)
+            
+        except Playlist.DoesNotExist:
+            return Response({'message': 'Playlist not found.'}, status=status.HTTP_404_NOT_FOUND)
         
-    # # works without authentication
-    # @api_view(['GET'])
-    # def get_playlist_by_id(request, id):
-    #     try:
-    #         playlist = Playlist.objects.get(pk=id)
-    #         serializer = PlaylistSerializer(playlist)
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     except Playlist.DoesNotExist:
-    #         return Response({'message': 'Playlist not found.'}, status=status.HTTP_404_NOT_FOUND)
-    #     except Exception as error:
-    #         return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as error:
+            return Response({'message': 'Failed to get playlist.', 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-    # # works without authentication
-    # @api_view(['DELETE'])
+   
     # def delete_playlist(request, id):
     #     try:
     #         playlist = Playlist.objects.get(pk=id)
