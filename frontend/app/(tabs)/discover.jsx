ef57cd12bb2d135a462950ef2f154b5b090c0b10
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-08 23:51:53 +0300

    frontend-styled-dropdown

--- frontend/app/(tabs)/discover.jsx
+++ frontend/app/(tabs)/discover.jsx
@@ -1,19 +1,21 @@
-import { View, Text, Image, SafeAreaView, FlatList } from 'react-native';
+import { View, Text, Image, SafeAreaView, FlatList, StyleSheet } from 'react-native';
 import React from 'react';
 import SelectDropdown from 'react-native-select-dropdown'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Discover = () => {
-  const genres = {
-    "genres": ["alternative", "samba"]
-  }
+  const genres = [{title: 'Classical'}, {title: 'Alternative'}]
+  // Genres api:
+  // {
+  // "genres": ["alternative", "samba"]
+  // }
 
   return (
     <SafeAreaView className='bg-white h-full pt-20 flex items-center'>
-      <View className='pt-10 bg-grey rounded'>
-          <Text className='font-urbanistBold text-base'>Generate your playlist now</Text>
-          <Text className='font-urbanist text-base'>Genre</Text>
+      <View className='pt-5 bg-grey rounded mx-7 w-72 px-2 h-40'>
+          <Text className='font-urbanistBold text-base self-center pb-1'>Generate your playlist now</Text>
 
+          <Text className='font-urbanist text-base'>Genre</Text>
           <SelectDropdown
             data={genres}
             onSelect={(selectedItem, index) => {
@@ -21,27 +23,23 @@ const Discover = () => {
             }}
             renderButton={(selectedItem, isOpened) => {
               return (
-                <View>
-                  {selectedItem && (
-                    <Icon name={selectedItem.icon}  />
-                  )}
-                  <Text >
-                    {(selectedItem && selectedItem.title) || 'Select your mood'}
+                <View style={styles.dropdownButtonStyle}>
+                  <Text className='font-urbanist text-base'>
+                    {(selectedItem && selectedItem.title) || 'Select your genre'}
                   </Text>
-                  <Icon name={isOpened ? 'chevron-up' : 'chevron-down'} />
+                  <Icon name={isOpened ? 'chevron-up' : 'chevron-down'} style={styles.dropdownButtonArrowStyle} />
                 </View>
               );
             }}
-            renderItem={(item, index, isSelected) => {
+            renderItem={(item) => {
               return (
-                <View >
-                  <Icon name={item.icon}  />
-                  <Text >{item.title}</Text>
+                <View style={styles.dropdownItemStyle}>
+                  <Text className='font-urbanist text-base'>{item.title}</Text>
                 </View>
               );
             }}
             showsVerticalScrollIndicator={false}
-            
+            dropdownStyle={styles.dropdownMenuStyle}
           />
       </View>
     </SafeAreaView>
@@ -49,3 +47,29 @@ const Discover = () => {
 }
 
 export default Discover;
+
+const styles = StyleSheet.create({
+  dropdownButtonStyle: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dropdownButtonArrowStyle: {
+    fontSize: 28,
+  },
+  dropdownMenuStyle: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+  },
+  dropdownItemStyle: {
+    width: '100%',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+});
