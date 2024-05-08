74b10211623c1abafc25311e7027bc8b7ecba84c
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-08 13:48:53 +0300

    backend-modified-user-model

--- backend/cadence/models/Usermodel.py
+++ backend/cadence/models/Usermodel.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User;
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = models.CharField(max_length=255, null=True)
-    cover_photo = models.CharField(max_length=255, null=True)
+    profile_photo_name = models.CharField(max_length=255, null=True)
+    profile_photo = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    cover_photo_name = models.CharField(max_length=255, null=True)
+    cover_photo = models.ImageField(upload_to='cover_images/', null=True, blank=True)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE, null=True)
 
