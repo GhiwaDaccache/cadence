56b562b8f76ac1dd0a7709331ac9b0eb632a40fb
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-08 14:32:11 +0300

    backend-created-upload-picture-view

--- backend/cadence/views/UserViews.py
+++ backend/cadence/views/UserViews.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..serializers.UserSerializer import UserProfileSerializer
 from ..models.Usermodel import UserProfile
+import time
     
 class RegistrationViews(APIView):
     permission_classes = [AllowAny]
@@ -19,7 +20,7 @@ class RegistrationViews(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    
 class UserViews(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -31,7 +32,28 @@ class UserViews(APIView):
             return Response(serializer.data)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+        
+    @api_view(['POST'])
+    def upload_image(request):
+        try:
+            user_profile = request.user.userprofile
+            image_file = request.data.get('image')
+
+            if image_file:
+                image_name = image_file.name
+                with open('cadence/media/profile_images/' + image_name, 'wb+') as destination:
+                    for chunk in image_file.chunks():
+                        destination.write(chunk)
+                
+                user_profile.profile_photo_name = image_name
+                user_profile.save()
+                
+                return Response({'message': 'Image uploaded successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'No image found'}, status=status.HTTP_400_BAD_REQUEST)
+            
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
 
     # def put(self, request):
     #     user_profile = request.user.userprofile
