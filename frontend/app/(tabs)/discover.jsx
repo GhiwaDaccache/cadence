284cb4c9898c3fce6b919cac2850678c566d6381
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-08 23:19:44 +0300

    frontent-created-generate-playlist-view

--- frontend/app/(tabs)/discover.jsx
+++ frontend/app/(tabs)/discover.jsx
@@ -1,12 +1,50 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, SafeAreaView, FlatList } from 'react-native';
 import React from 'react';
-
+import SelectDropdown from 'react-native-select-dropdown'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Discover = () => {
-  return (
-    <View>
+  const genres = {
+    "genres": ["alternative", "samba"]
+  }
 
-    </View>
+  return (
+    <SafeAreaView className='bg-white h-full pt-20 flex items-center'>
+      <View className='pt-10 bg-grey rounded'>
+          <Text className='font-urbanistBold text-base'>Generate your playlist now</Text>
+          <Text className='font-urbanist text-base'>Genre</Text>
+
+          <SelectDropdown
+            data={genres}
+            onSelect={(selectedItem, index) => {
+              console.log(selectedItem, index);
+            }}
+            renderButton={(selectedItem, isOpened) => {
+              return (
+                <View>
+                  {selectedItem && (
+                    <Icon name={selectedItem.icon}  />
+                  )}
+                  <Text >
+                    {(selectedItem && selectedItem.title) || 'Select your mood'}
+                  </Text>
+                  <Icon name={isOpened ? 'chevron-up' : 'chevron-down'} />
+                </View>
+              );
+            }}
+            renderItem={(item, index, isSelected) => {
+              return (
+                <View >
+                  <Icon name={item.icon}  />
+                  <Text >{item.title}</Text>
+                </View>
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            
+          />
+      </View>
+    </SafeAreaView>
   )
 }
 
