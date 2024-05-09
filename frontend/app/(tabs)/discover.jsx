57bf9bff96980abce59090d23f39fe745ee9f423
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-09 11:41:07 +0300

    frontend-styled-generate-playlist

--- frontend/app/(tabs)/discover.jsx
+++ frontend/app/(tabs)/discover.jsx
@@ -1,22 +1,35 @@
-import { View, Text, Image, SafeAreaView, FlatList, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, Image, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import GenreCard from '../../components/GenreCard';
 
 const Discover = () => {
-  const genres = [{title: 'Classical'}, {title: 'Alternative'}]
   // Genres api:
-  // {
-  // "genres": ["alternative", "samba"]
-  // }
+  const genres = {"genres": ["alternative", "samba", "classical", "french", "pop", "rock" ]}
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
+  const renderGenreCard = ({ item }) => (
+    <TouchableOpacity onPress={() => setSelectedGenre(item)}>
+      <GenreCard
+        genre={item}
+        isSelected={selectedGenre === item}
+      />
+    </TouchableOpacity>
+  )
 
   return (
     <SafeAreaView className='bg-white h-full pt-20 flex items-center'>
-      <View className='pt-5 bg-grey rounded mx-7 w-72 px-2 h-40'>
+      <View className='pt-3 bg-grey rounded mx-7 w-72 px-2'>
           <Text className='font-urbanistBold text-base self-center pb-1'>Generate your playlist now</Text>
 
           <Text className='font-urbanist text-base pb-2'>Genre</Text>
-          <View className='h-7 bg-darkGrey rounded-md flex justify-center items-center'>
-            <Text className='font-urbanist'>Classical</Text>
-          </View>
+          
+          <FlatList
+            data={genres.genres}
+            renderItem={renderGenreCard}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item, index) => index.toString()}
+          />
 
           <Text className='font-urbanist text-base pt-2'>Intervals</Text>
 
--- frontend/components/GenreCard.jsx
+++ frontend/components/GenreCard.jsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 
 const GenreCard = ({ genre }) => {
   return (
-    <View className='h-7 bg-darkGrey rounded-md flex justify-center items-center'>
+    <View className='h-7 bg-darkGrey rounded-md flex justify-center items-center px-2 mx-1'>
         <Text className='font-urbanist'>{genre}</Text>
     </View>
   )
