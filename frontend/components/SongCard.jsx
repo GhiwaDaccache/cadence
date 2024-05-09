ab178f063ab4405fb3d6a1bc87fc0043f27b600c
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-09 18:44:11 +0300

    frontend-created-SongCard-component

---
+++ frontend/components/SongCard.jsx
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const SongCard = () => {
+  return (
+    <View>
+      <Text>SongCard</Text>
+    </View>
+  )
+}
+
+export default SongCard;
