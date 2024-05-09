9bc9bd2942b1e3e5c1d596c60d481cb1e148c3cd
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-09 18:51:37 +0300

    frontend-added-SongCard-styling

--- frontend/components/SongCard.jsx
+++ frontend/components/SongCard.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 
-const SongCard = () => {
+const SongCard = ({ image, songName, artistName }) => {
   return (
-    <View>
-      <Text>SongCard</Text>
-    </View>
+    <View className='flex flex-row pt-6'>
+        <Image 
+          source={image}
+        />
+        <View className='flex justify-center pl-4'>
+          <Text className='font-urbanistBold text-base'>{songName}</Text>
+          <Text className=' pt-3 font-urbanist'>{artistName}</Text>
+        </View>
+      </View>
   )
 }
 
