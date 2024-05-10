5271c06e80413dd74ab7fbdae079ab4f715814f1
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-10 10:55:49 +0300

    frontend-changed-timer-logic

--- frontend/components/Timer.jsx
+++ frontend/components/Timer.jsx
@@ -1,35 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 const Timer = () => {
-    const [currentTime, setCurrentTime] = useState(new Date().setHours(0, 0, 0, 0)); // Start time initialized to 00:00:00
+  const [time, setTime] = useState(0);
 
-    useEffect(() => {
-        const timerID = setInterval(() => {
-            setCurrentTime(prevTime => new Date(prevTime.getTime() + 1000)); // Increment current time by 1 second
-        }, 1000);
-        return () => clearInterval(timerID);
-    }, []);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(time + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [time]);
 
-    const formattedTime = () => {
-        const date = new Date(currentTime);
-        const hours = date.getHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        const seconds = date.getSeconds().toString().padStart(2, '0');
-        return `${hours}:${minutes}:${seconds}`;
-    };
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
 
-    return (
-        <View>
-            <Text className='font-urbanistBold text-2xl pt-4'>{formattedTime()}</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Text className='font-urbanistBold text-2xl pt-4'>{formatTime(time)}</Text>
+    </View>
+  );
 };
 
-export default Timer
-
-
-
-
-
-  
+export default Timer;
