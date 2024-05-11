958bc32260c810b0722f40d6e08f2068f7de6f73
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-11 10:07:39 +0300

    frontend-modified-PlaylistCard-component

--- frontend/app/playlist/[id].js
+++ frontend/app/playlist/[id].js
@@ -1,5 +1,7 @@
 import { View, Text,  } from "react-native";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
+import PlaylistCard from "../../components/PlaylistCard";
+import images from '../../assets/images/images'; 
 
 
 export default function playlistDetails() {
@@ -33,14 +35,19 @@ export default function playlistDetails() {
           }
         ]}
 
-    const router = useRouter();
     const { id } = useLocalSearchParams();
-
     const selectedPlaylist = playlists.data.find((item) => { return item.playlist.id == id})
 
     return (
-        <View>
-            <Text>{selectedPlaylist.name}</Text>
+        <View className='h-full bg-white'>
+            <View className='self-center'>
+                <PlaylistCard
+                    title={selectedPlaylist.playlist.name}
+                    image={images.playlist}
+                    level={selectedPlaylist.playlist.level}
+                    time={'20:18'}
+                />
+            </View>
         </View>
 
     )
--- frontend/components/PlaylistCard.jsx
+++ frontend/components/PlaylistCard.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
-const PlaylistCard = ({ image, title, time, level, handlePress }) => {
+const PlaylistCard = ({ image, title, time, level, handlePress, imageSize }) => {
   return (
     <TouchableOpacity className='flex flex-row pt-6' onPress={handlePress}>
         <Image 
+          className={imageSize}
           source={image}
         />
         <View className='flex justify-center pl-4'>
