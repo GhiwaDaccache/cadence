26fb9f5e899e25e44adabf07a194a9852c052924
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-13 18:31:00 +0300

    frontend-added-music-player

--- frontend/app/(tabs)/profile/logic/music-player-logic.js
+++ frontend/app/(tabs)/profile/logic/music-player-logic.js
@@ -2,34 +2,64 @@ import { useEffect, useState } from 'react';
 import { Audio } from 'expo-av';
 
 export const useMusicLogic = () => {
-  const [sound, setSound] = useState({});
+//   const [sound, setSound] = useState({});
 
-  const playSound = () => {
-    console.log('test')
+    const onPlayBackStatusUpdate = (status) =>
+        {
+            console.log(status)
+        }
+  async function playSound() {
+    try{ 
+        console.log('Loading Sound');
+        await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
+
+        const { sound } = await Audio.Sound.createAsync(
+            { uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3' },
+            { shouldPlay: true },
+            onPlayBackStatusUpdate
+          );
+
+        // const sound = new Audio.Sound()
+        // console.log('Sound ');
+        // await sound.loadAsync(
+        //     {uri: "https://open.spotify.com/track/11dFghVXANMlKmJXsNCbNl"},
+        //     { shouldPlay: true }
+        // )
+
+        
+        // await sound.playAsync()
+
+        // setSound({ sound: playbackObject });
+
+        console.log('Playing Sound');
+        // await sound.playAsync();
     }
-//     console.log('Loading Sound');
-//     await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
-//     const { sound: playbackObject } = await Audio.Sound.createAsync(
-//         { uri: 'https://api.spotify.com/v1/tracks/1JGWAEOOPv8LyKh92eVa39' },
-//         { shouldPlay: true }
-//       );
-//     setSound({sound: playbackObject});
+    catch(err){
+       // handle rejection
+       console.error(err)
+    }
+    
+  }
 
-//     console.log('Playing Sound');
-//     await sound.playAsync();
-//   }
+  useEffect(() => {
+    playSound;
+  }, []);
 
 //   useEffect(() => {
 //     return sound
 //       ? () => {
 //           console.log('Unloading Sound');
-//         //   sound.unloadAsync();
+//           sound.unloadAsync();
 //         }
 //       : undefined;
 //   }, [sound]);
+
 
   return{
     playSound
     }
   
 }
+
+
+
