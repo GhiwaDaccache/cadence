769b9fc7451f29a87cf6e47fdb755b6b7307753d
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-13 19:20:38 +0300

    frontend-updated-music-player

--- frontend/app/(tabs)/profile/logic/music-player-logic.js
+++ frontend/app/(tabs)/profile/logic/music-player-logic.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Audio } from 'expo-av';
+import { getValueFor } from '../../../../tools/secureStore';
 
 export const useMusicLogic = () => {
 //   const [sound, setSound] = useState({});
@@ -12,12 +13,54 @@ export const useMusicLogic = () => {
     try{ 
         console.log('Loading Sound');
         await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
+        const spotifyToken = await getValueFor('spotify-token')
+        // const { sound } = await Audio.Sound.createAsync(
+        //     { uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3' },
+        //     { shouldPlay: true },
+        //     onPlayBackStatusUpdate
+        //   );
 
-        const { sound } = await Audio.Sound.createAsync(
-            { uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3' },
-            { shouldPlay: true },
-            onPlayBackStatusUpdate
-          );
+        const data = {
+            uris: ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:1301WleyT98MSxVHPZCA6M'],
+            position_ms: 0
+          };
+
+          fetch("https://api.spotify.com/v1/me/player/play?device_id=425d882af17f4f94d09209e227264c899a7172dd", {
+            method: 'PUT',
+            headers: {
+              'Authorization': 'Bearer ' + accessToken,
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok. Status: ' + response.status);
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+
+        // const resp = await
+        //   fetch(`https://api.spotify.com/v1/me/player/play?device_id=425d882af17f4f94d09209e227264c899a7172dd`, {
+        //             method: "PUT", 
+        //             headers: {
+        //                 Authorization: `Bearer ${spotifyToken}`,
+        //             },
+        //             body: {
+        //                 "uris": ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh", "spotify:track:1301WleyT98MSxVHPZCA6M"],
+        //                 "position_ms": 0
+        //             }
+        //         });
+                // .then(response => {
+                //     console.log(response.json())
+                // })
+
 
         // const sound = new Audio.Sound()
         // console.log('Sound ');
