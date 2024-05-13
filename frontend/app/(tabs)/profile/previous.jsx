513b0a37c83b5edaffe3c1642d1a4f7320b48a87
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-13 11:00:09 +0300

    frontend-moved-renderPrevious-logic

--- frontend/app/(tabs)/profile/previous.jsx
+++ frontend/app/(tabs)/profile/previous.jsx
@@ -1,28 +1,16 @@
 // Dependencies
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View } from 'react-native';
  
 // Components
-import Run from '../../../components/Run';
 import { usePrviousLogic } from './logic/previous-logic';
 
 const previous = () => {
-  const { previousRuns, setPreviousRuns } = usePrviousLogic();
+  const { renderPreviousRuns } = usePrviousLogic();
 
   return (
     <View className='h-full bg-white flex items-center pt-3'>
-      <FlatList 
-        data = {previousRuns}
-        keyExtractor={(item) => item.id}
-        renderItem={({item})=> (
-          <Run
-          date={item.recorded_on}
-          distance={item.real_distance}
-          pace={item.real_pace}
-          time={item.real_duration}
-          />
-        )}
-      />
+      {renderPreviousRuns()}
     </View>
   )
 }
