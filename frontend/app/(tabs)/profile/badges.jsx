d17fdd8b80d336b2c885b30364c0c2f4e1552fbf
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-14 16:16:00 +0300

    frontend-implement-badges-hook

--- frontend/app/(tabs)/profile/badges.jsx
+++ frontend/app/(tabs)/profile/badges.jsx
@@ -8,41 +8,15 @@ import Badge from '../../../components/Badge';
 // Assets
 import icons from '../../../assets/icons/icons';
 
+// Custom hooks
+import { useBadgeLogic } from './logic/badges-logic';
+
 const Badges = () => {
+  const { renderBadges } = useBadgeLogic();
+  
   return (
     <View className='h-full bg-white flex items-center pt-3'>
-      <Badge
-        title={'First run'}
-        icon={icons.trophy}
-        date={"00-00-2000"}
-        distance={"00.00"}
-        time={"00:00"}
-      />
-
-      <Badge
-        title={'Early bird'}
-        icon={icons.sunrise}
-        date={"00-00-2000"}
-        distance={"00.00"}
-        time={"00:00"}
-      />
-
-      <Badge
-        title={'Longest distance'}
-        icon={icons.distance}
-        date={"00-00-2000"}
-        distance={"00.00"}
-        time={"00:00"}
-      />
-
-      <Badge
-        title={'Longest duration'}
-        icon={icons.timer}
-        date={"00-00-2000"}
-        distance={"00.00"}
-        time={"00:00"}
-      />
-
+      {renderBadges()}
     </View>
   )
 }
