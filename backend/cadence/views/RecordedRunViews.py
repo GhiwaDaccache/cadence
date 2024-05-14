319e23548ec80e135e2a52dc112b817317efeefa
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-14 13:23:48 +0300

    backend-added-first-badge-logic

--- backend/cadence/views/RecordedRunViews.py
+++ backend/cadence/views/RecordedRunViews.py
@@ -7,7 +7,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication;
 
 # Models
 from ..models.RecordedRunModel import RecordedRun;
-
+from ..models.BadgeModel import Badge;
+from ..models.EarnedBadgeModel import EarnedBadge;
 # Serializers
 from ..serializers.RecordedRunSerializer import RecordedRunSerializer;
 
@@ -23,7 +24,14 @@ class RecordedRunViews(APIView):
             serializer = RecordedRunSerializer(data=request.data)
             
             if serializer.is_valid():
-                serializer.save()
+                recorded_run = serializer.save()
+                user_run_count = RecordedRun.objects.filter(user=user_id).count()
+
+                if user_run_count == 1:
+                    first_badge = Badge.objects.get(name='First Run')
+                    EarnedBadge.objects.create(badge=first_badge, recorded_run=recorded_run)
+
+
                 return Response({'message': 'Run added successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
             return Response({'message': 'Failed to add run', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
