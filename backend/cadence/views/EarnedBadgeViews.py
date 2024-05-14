fda034166c4290211be4f189fa34bb9c06f41942
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-14 13:39:57 +0300

    backend-created-get-earned-badge-api

--- backend/cadence/views/EarnedBadgeViews.py
+++ backend/cadence/views/EarnedBadgeViews.py
@@ -10,6 +10,7 @@ from ..models.EarnedBadgeModel import EarnedBadge;
 
 # Serializers
 from ..serializers.EarnedBadgeSerializer import EarnedBadgeSerializer;
+from ..serializers.RecordedRunSerializer import RecordedRunSerializer;
 
 
 class EarnedBadgeViews(APIView):
@@ -28,11 +29,28 @@ class EarnedBadgeViews(APIView):
         except Exception as error:
             return Response({'message': 'Failed to add earned badge', 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
+    def get(self, request):
+        try:
+            user_id = request.user.id
+            earned_badges = EarnedBadge.objects.filter(recorded_run__user_id = user_id)
+            run_data = []
+
+            for badge in earned_badges:
+                earned_badge = EarnedBadgeSerializer(badge).data
+                recorded_run = RecordedRunSerializer(badge.recorded_run).data
+                earned_badge['recorded_run'] = recorded_run
+                run_data.append(earned_badge)
+
+            return Response({'message': 'Success', 'data': run_data}, status=status.HTTP_200_OK)
+        
+        except Exception as error:
+            return Response({'message': 'Failed to get badges', 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def delete(self, request):
         try:
             user_id = request.user.id
             badge_id = request.data.get('badge')
-            earned_badge = EarnedBadge.objects.filter(recorded_run__user_id=user_id, badge_id=badge_id).first()
+            earned_badge = EarnedBadge.objects.filter(recorded_run__user_id = user_id, badge_id = badge_id).first()
 
             if earned_badge:
                 earned_badge.delete()  
