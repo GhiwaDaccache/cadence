f54c3e3d8e18cb0a246c2dfacfd075e4886e87cd
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-14 12:28:17 +0300

    frontend-added-spotify-auth-session

--- frontend/app/(tabs)/profile/logic/test-logic.js
+++ frontend/app/(tabs)/profile/logic/test-logic.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React from 'react';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+
 
 export const useTestLogic = () => {
+
+    // const discovery = {
+    //     authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+    //     tokenEndpoint: 'https://accounts.spotify.com/api/token',
+    //   };
+      
+    //   export default function App() {
+    //     const [request, response, promptAsync] = useAuthRequest(
+    //       {
+    //         clientId: 'CLIENT_ID',
+    //         scopes: ['user-read-email', 'playlist-modify-public'],
+    //         // To follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+    //         // this must be set to false
+    //         usePKCE: false,
+    //         redirectUri: makeRedirectUri({
+    //           scheme: 'your.app'
+    //         }),
+    //       },
+    //       discovery
+    //     );
+      
+    //     React.useEffect(() => {
+    //       if (response?.type === 'success') {
+    //         const { code } = response.params;
+    //       }
+    //     }, [response]);
+      
+    //     return (
+    //       <Button
+    //         disabled={!request}
+    //         title="Login"
+    //         onPress={() => {
+    //           promptAsync();
+    //         }}
+    //       />
+    //     );
+    //   }
     const authenticate = async () =>{
         const config = {
             issuer: 'https://accounts.spotify.com',
@@ -12,10 +51,13 @@ export const useTestLogic = () => {
                 'user-modify-playback-state',
                 'user-read-currently-playing',
                 'user-read-private'
-            ]
-
+            ],
+        redirectUrl: 'https://www.google.com/'
         }
+        const result = await AppAuth.authAsync(config)
+        console.log(result)
     }
+
     return{
         
         }
