6bd872a289ec9ff7b95e2677cde6e6414454e77c
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-15 10:21:51 +0300

    backend-created-get-plan-details

--- backend/cadence/serializers/RunSerializer.py
+++ backend/cadence/serializers/RunSerializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from ..models.RunModel import Run
+from ..serializers.PlanRunSerializer import PlanRunSerializer
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = ['id', 'playlist', 'avg_pace', 'duration', 'distance']
+
+class RunPlanSerializer(serializers.Serializer):
+    run = RunSerializer()
+    plan_run = PlanRunSerializer()
--- backend/cadence/views/PlanRunViews.py
+++ backend/cadence/views/PlanRunViews.py
@@ -6,10 +6,14 @@ from rest_framework.permissions import IsAuthenticated;
 from rest_framework_simplejwt.authentication import JWTAuthentication;
 
 # Models
+from ..models.RunModel import Run;
 from ..models.PlanRunModel import PlanRun;
+from ..models.RecordedRunModel import RecordedRun;
 
 # Serializers
+from ..serializers.RunSerializer import RunSerializer;
 from ..serializers.PlanRunSerializer import PlanRunSerializer;
+from ..serializers.RecordedRunSerializer import RecordedRunSerializer;
 
 
 class PlanRunViews(APIView):
@@ -38,5 +42,31 @@ class PlanRunViews(APIView):
         
         except Exception as error:
             return Response({'message': 'Failed to update', 'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def get(self, request):
+        try:
+            user = request.user
+            last_recorded_run = RecordedRun.objects.filter(user = user, run__isnull = False).latest('recorded_on')
+            run_id = last_recorded_run.run_id
 
+            plan_run = PlanRun.objects.get(run_id = run_id)
+            week = plan_run.week
+            plan_id = plan_run.plan_id
+
+            runs = Run.objects.filter(planrun__plan_id = plan_id)
+            run_serializer = RunSerializer(runs, many = True)
+            plan_runs = PlanRun.objects.filter(plan_id = plan_id, week = week)
+            plan_run_serializer = PlanRunSerializer(plan_runs, many = True)
+            recorded_run_serializer = RecordedRunSerializer(last_recorded_run)
+
+            data = {
+                'last_recorded_run': recorded_run_serializer.data,
+                'runs': run_serializer.data,
+                'plan_runs': plan_run_serializer.data
+            }
+            
+            return Response({'message': 'Success', 'data': data}, status=status.HTTP_200_OK)
+        
+        except Exception as error:
+            return Response({'message': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
    
