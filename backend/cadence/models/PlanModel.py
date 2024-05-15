0a0c942f20b210cd397fec8b015b5d9b7947afa3
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-05-15 10:36:52 +0300

    backend-modified-plan-model

--- backend/cadence/models/PlanModel.py
+++ backend/cadence/models/PlanModel.py
@@ -4,6 +4,7 @@ from django.core.validators import MinValueValidator;
 class Plan(models.Model):
     name = models.CharField(max_length=255)
     duration = models.IntegerField(validators=[MinValueValidator(0)])
+    distance = models.IntegerField(validators=[MinValueValidator(0)])
     level = models.CharField(max_length=100)
     image = models.CharField(max_length=255)
 
